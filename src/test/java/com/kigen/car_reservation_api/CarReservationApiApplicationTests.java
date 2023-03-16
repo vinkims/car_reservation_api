c3b576b49bdb16fa5fd96cf83b8d3a6aab7153f8
vinkims <vincekigen@gmail.com>
2023-03-16 12:17:30 +0300

    chore(Setup)- Initial project setup

---
+++ src/test/java/com/kigen/car_reservation_api/CarReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kigen.car_reservation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
